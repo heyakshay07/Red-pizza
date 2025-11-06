b20048d4e2db2a705c1ca4a85d446407a193f852
deeptigujar <deeptigujar8200@gmail.com>
2025-11-06 17:36:10 +0530

    changesh

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Coupens from './components/Coupens'
 import Footer from './components/Footer'
+import  Menu  from './components/Menu'
 
 const App = () => {
   return (
     <div >
       <Navbar/>
       <Coupens/>
+      <Menu/>
       <Footer/>
     </div>
   )
